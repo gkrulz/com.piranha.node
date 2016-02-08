f5bc23a99276bad6b034ce14ab87f73850365434
Padmaka <padmaka.jw@gmail.com>
2016-02-08 22:14:56 +0530

    Dependency sending ad receiving implemented

--- src/main/java/com/piranha/node/Bootstrap.java
+++ src/main/java/com/piranha/node/Bootstrap.java
@@ -2,11 +2,14 @@ package com.piranha.node;
 
 import com.piranha.node.compile.CompilationListener;
 import com.piranha.node.compile.DependencyProvider;
+import com.piranha.node.constants.Constants;
 import com.piranha.node.util.Communication;
 import org.apache.log4j.Logger;
 
 import java.io.*;
+import java.net.InetAddress;
 import java.net.Socket;
+import java.net.UnknownHostException;
 import java.nio.charset.StandardCharsets;
 import java.util.Properties;
 
@@ -17,6 +20,14 @@ public class Bootstrap {
     private static final Logger log = Logger.getLogger(Bootstrap.class);
 
     public static void main(String[] args) {
+        InetAddress localIpAddress= null;
+        try {
+            localIpAddress = InetAddress.getLocalHost();
+        } catch (UnknownHostException e) {
+            log.error("Error", e);
+        }
+        log.info("IP: " + localIpAddress);
+
         Communication comm = new Communication();
         Properties properties = new Properties();
         try {
