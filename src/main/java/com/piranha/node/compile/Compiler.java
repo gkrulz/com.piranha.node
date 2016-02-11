30320667ed9957d9bab3b55c7dbf421fd17fcbc8
Padmaka <padmaka.jw@gmail.com>
2016-02-11 22:22:53 +0530

    Dependency sending ad receiving implemented

--- src/main/java/com/piranha/node/compile/Compiler.java
+++ src/main/java/com/piranha/node/compile/Compiler.java
@@ -109,20 +109,24 @@ public class Compiler extends Thread {
     }
 
     private boolean isCompletelyWritten(File file) {
-        RandomAccessFile stream = null;
-        try {
-            stream = new RandomAccessFile(file, "rw");
-            return true;
-        } catch (Exception e) {
-            log.info("Skipping file " + file.getName() + " for this iteration due it's not completely written");
-        } finally {
-            if (stream != null) {
-                try {
-                    stream.close();
-                } catch (IOException e) {
-                    log.error("Exception during closing file " + file.getName());
+        if (file.exists()) {
+            RandomAccessFile stream = null;
+            try {
+                stream = new RandomAccessFile(file, "rw");
+                return true;
+            } catch (Exception e) {
+                log.info("Skipping file " + file.getName() + " for this iteration due it's not completely written");
+            } finally {
+                if (stream != null) {
+                    try {
+                        stream.close();
+                    } catch (IOException e) {
+                        log.error("Exception during closing file " + file.getName());
+                    }
                 }
             }
+        } else {
+            return false;
         }
         return false;
     }
